b58e6184076a71ba24f4e4d53a33feb27f6f403e
aidenkiefer <akief3@uic.edu>
2026-02-13 14:52:34 -0600

    update package

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  // Required for next-mdx-remote v6 with Turbopack (see next-mdx-remote README)
+  transpilePackages: ['next-mdx-remote'],
 };
 
 export default nextConfig;
